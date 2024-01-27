e977dcee05555f92658f562d9b67622ce923ca96
Zubair Shaikh <zsescape@gmail.com>
2024-01-27 18:56:34 -0500

    Update layout - Add copy.js - Update main.py - Update styles.css

---
+++ assets/js/copy.js
@@ -0,0 +1,16 @@
+// const toastLiveExample = document.getElementById("liveToast")
+// const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+//
+// const decryptedText = document.getElementById("decrypted-text")
+// decryptedText.addEventListener("click", () => {
+//     navigator.clipboard.writeText(decryptedText.value)
+//     toastBootstrap.show()
+// })
+//
+// const encryptedText = document.getElementById("encrypted-text")
+// encryptedText.addEventListener("click", () => {
+//     navigator.clipboard.writeText(encryptedText.value)
+//     toastBootstrap.show()
+// })
+
+const _0x58e64c=_0x114d;(function(_0xb64bef,_0xec824d){const _0x56c14c=_0x114d,_0x2503d7=_0xb64bef();while(!![]){try{const _0x8d88c6=parseInt(_0x56c14c(0x119))/0x1*(parseInt(_0x56c14c(0x11c))/0x2)+-parseInt(_0x56c14c(0x11d))/0x3+-parseInt(_0x56c14c(0x128))/0x4*(-parseInt(_0x56c14c(0x124))/0x5)+-parseInt(_0x56c14c(0x118))/0x6+parseInt(_0x56c14c(0x123))/0x7+parseInt(_0x56c14c(0x117))/0x8+parseInt(_0x56c14c(0x11a))/0x9*(-parseInt(_0x56c14c(0x11f))/0xa);if(_0x8d88c6===_0xec824d)break;else _0x2503d7['push'](_0x2503d7['shift']());}catch(_0xac4b0b){_0x2503d7['push'](_0x2503d7['shift']());}}}(_0xf30c,0x9b5ec));const toastLiveExample=document[_0x58e64c(0x11b)](_0x58e64c(0x11e)),toastBootstrap=bootstrap[_0x58e64c(0x122)][_0x58e64c(0x126)](toastLiveExample),decryptedText=document[_0x58e64c(0x11b)](_0x58e64c(0x114));function _0x114d(_0x4ae536,_0x1b2d28){const _0xf30cd=_0xf30c();return _0x114d=function(_0x114d2f,_0x462f0a){_0x114d2f=_0x114d2f-0x114;let _0x624ae0=_0xf30cd[_0x114d2f];return _0x624ae0;},_0x114d(_0x4ae536,_0x1b2d28);}decryptedText['addEventListener'](_0x58e64c(0x115),()=>{const _0x183279=_0x58e64c;navigator[_0x183279(0x121)][_0x183279(0x127)](decryptedText[_0x183279(0x116)]),toastBootstrap[_0x183279(0x125)]();});const encryptedText=document[_0x58e64c(0x11b)](_0x58e64c(0x129));function _0xf30c(){const _0x321351=['874806FLzhoQ','toast','10iQkOmd','addEventListener','clipboard','Toast','6574484oEkKIT','45995CsKcJk','show','getOrCreateInstance','writeText','12kCjQjh','encrypted-text','decrypted-text','click','value','5643600jcCFeg','718710PrtRGM','13GXbIFf','16584237gzvgqX','getElementById','187418jkTVvl'];_0xf30c=function(){return _0x321351;};return _0xf30c();}encryptedText[_0x58e64c(0x120)](_0x58e64c(0x115),()=>{const _0x5c82c4=_0x58e64c;navigator[_0x5c82c4(0x121)][_0x5c82c4(0x127)](encryptedText['value']),toastBootstrap[_0x5c82c4(0x125)]();});
--- main.py
+++ main.py
@@ -1,33 +1,55 @@
 from pyweb import pydom
-from random import randint
+from string import ascii_lowercase, ascii_uppercase
+
+
+def displace(text, displacement):
+    result = ""
+    for character in text:
+        if character in ascii_lowercase:
+            result += chr((ord(character) - ord('a') + displacement) % 26 + ord('a'))
+        elif character in ascii_uppercase:
+            result += chr((ord(character) - ord('A') + displacement) % 26 + ord('A'))
+        else:
+            result += character
+    return result
+
+
+def encrypt(event):
+    textarea_element = pydom["textarea#text-to-encrypt"][0]
+    text = textarea_element.value
+
+    key_element = pydom["input#encrypt-key-input"][0]
+    key = int(key_element.value)
+
+    decrypted_text_element = pydom["textarea#encrypted-text"][0]
+    decrypted_text_element.value = displace(text, key)
 
 
 def decrypt(event):
     textarea_element = pydom["textarea#text-to-decrypt"][0]
-    print(textarea_element.value)
+    text = textarea_element.value
+
+    key_element = pydom["input#decrypt-key-input"][0]
+    key = int(key_element.value)
 
     decrypted_text_element = pydom["textarea#decrypted-text"][0]
-    decrypted_text_element.value = "{}-{}".format(textarea_element.value, randint(1, 100))
-
-    decrypted_text_container_element = pydom["div#decrypted-text-container"][0]
-    decrypted_text_container_element.visible = True
+    decrypted_text = displace(text, -key)
+    decrypted_text_element.value = decrypted_text
 
 
 def sync_range_to_input(event):
-    key_range_element = pydom["input#key-range"][0]
+    prefix = event.target.id.split("-")[0]
+    key_range_element = pydom["input#{}-key-range".format(prefix)][0]
     key = key_range_element.value
 
-    print(key)
-
-    key_element = pydom["input#key"][0]
+    key_element = pydom["input#{}-key-input".format(prefix)][0]
     key_element.value = key
 
 
 def sync_input_to_range(event):
-    key_element = pydom["input#key"][0]
+    prefix = event.target.id.split("-")[0]
+    key_element = pydom["input#{}-key-input".format(prefix)][0]
     key = key_element.value
 
-    print(key)
-
-    key_range_element = pydom["input#key-range"][0]
+    key_range_element = pydom["input#{}-key-range".format(prefix)][0]
     key_range_element.value = int(key) % 26
